15dcd62e2860084405a2726ccf115f6819c55359
Fateenwajahat <fateenwajahatj@gmail.com>
2024-07-22 19:03:28 +0530

    update TriangleAreaFinder.java

--- TriangleAreaFinder.java
+++ TriangleAreaFinder.java
@@ -3,7 +3,7 @@ public class TriangleAreaFinder {
 	public static void main(String[] args) {
         float height, base;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Press 1 for using Formula for calculating area\n press 2 for using Herons formula to calculate area:\nPress any other number to exit");
+        System.out.println("Press 1 for using Formula for calculating area\nPress 2 for using Herons formula to calculate area:\nPress any other number to exit");
         int choice = scanner.nextInt();
             switch (choice) {
                 case 1 -> {
@@ -16,9 +16,11 @@ public class TriangleAreaFinder {
                         scanner.close();
                     }
                 case 2 -> {
-                        System.out.println("Enter the side lengths of the triangle: ");
+                        System.out.println("Enter the length of first side of the triangle(a): ");
                         float a = scanner.nextFloat();
+                        System.out.println("Enter the length of second side of the triangle(b): ");
                         float b = scanner.nextFloat();
+                        System.out.println("Enter the length of third side of the triangle(c): ");
                         float c = scanner.nextFloat();
                         double s = (a + b + c) / 2;
                         double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
